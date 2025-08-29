02272aae6677ee6cd2f1feb7bbe55a3e8851e743
Thanveer Ali <128396991+Thanveerali1@users.noreply.github.com>
2025-08-29 14:44:06 +0530

    Add files via upload

---
+++ singlylinkedlist.py
@@ -0,0 +1,56 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last = self.head
+        while last.next:
+            last = last.next
+        last.next = new_node
+
+    def delete(self, key):
+        temp = self.head
+
+        if temp and temp.data == key:
+            self.head = temp.next
+            temp = None
+            return
+
+        prev = None
+        while temp and temp.data != key:
+            prev = temp
+            temp = temp.next
+
+        if temp is None:
+            return
+
+        prev.next = temp.next
+        temp = None
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+# Example usage
+if __name__ == "__main__":
+    sll = SinglyLinkedList()
+    sll.insert(10)
+    sll.insert(20)
+    sll.insert(30)
+    print("Linked List after inserts:")
+    sll.display()
+    sll.delete(20)
+    print("Linked List after deleting 20:")
+    sll.display()
